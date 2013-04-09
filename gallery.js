b9bae27433a9ec47213885e95bcd6479faef69e4
Vincent Leung <vincentleung1991@gmail.com>
2013-04-09 19:26:15 +0200

    basic function done

--- gallery.js
+++ gallery.js
@@ -1,12 +1,24 @@
-function swapImages(){
-	var $active = $('#img-list li.active');
-	var $next = ($('#img-list li.active').next().length > 0) ? $('#img-list li.active').next() : $('#img-list li:first');
-	$active.removeClass('active');
-	$next.addClass('active');
-	var largeImgSrc = $next.children("img").attr("src").replace("-", "").replace(".jpg", "").concat("-large.jpg");
-	$("#center-img").attr("src", largeImgSrc);
+function autoSlideShow(){
+	nextImage($("#img-list li.active"));
 }
 
+function nextImage(currentImg) {
+	var nextImg = (currentImg.next().length > 0) ? currentImg.next() : $('#img-list li:first');
+	currentImg.removeClass("active");
+	setCenterImg(nextImg);
+}
+
+function prevImage(currentImg) {
+	var prevImg = (currentImg.prev().length > 0) ? currentImg.prev() : $('#img-list li:last');
+	currentImg.removeClass("active");
+	setCenterImg(prevImg);
+}
+
+function setCenterImg(thumbnail) {
+	thumbnail.toggleClass("active", true);
+	var largeImgSrc = thumbnail.children("img").attr("src").replace("-", "").replace(".jpg", "").concat("-large.jpg");
+	$("#center-img").attr("src", largeImgSrc);
+}
 
 $(document).ready(function() {
 	var slideShowInterval = 2000;
@@ -18,26 +30,47 @@ $(document).ready(function() {
 	
 	//1.automatic slideshow
 	var intervalID;
+	var autoPlay = true;
 		
 	$("#play-pause-button").click(function() {
 		if ($(this).hasClass("ui-icon-play")) {
-			intervalID = setInterval("swapImages()", slideShowInterval);
+			intervalID = setInterval("autoSlideShow()", slideShowInterval);
 			$(this).removeClass("ui-icon-play");
 			$(this).addClass("ui-icon-pause");
+			autoPlay = true;
 		} else if ($(this).hasClass("ui-icon-pause")) {
 			clearInterval(intervalID);
 			$(this).addClass("ui-icon-play");
 			$(this).removeClass("ui-icon-pause");
+			autoPlay = false;
 		}
 	});
 	
 	//2.thumbmail
-	$(".photo-group img").click(function(){
-		var $newCenter = $(this).attr("src");
-		$newCenter = $newCenter.split(".")[0]
-		$newCenter = $newCenter.concat("-large.jpg")
-		$(".center").attr("src",$newCenter);			
+	$("#img-list li").click(function(){
+		clearInterval(intervalID);
+		$("#img-list li.active").removeClass("active");
+		setCenterImg($(this));
+		if (autoPlay){
+			intervalID = setInterval("autoSlideShow()", slideShowInterval);
+		}
 	});
 	
-	intervalID = setInterval('swapImages()', slideShowInterval);
+	//back and forward
+	$("#back-button").click(function() {
+		clearInterval(intervalID);
+		prevImage($("#img-list li.active"));
+		if (autoPlay){
+			intervalID = setInterval("autoSlideShow()", slideShowInterval);
+		}
+	});
+	$("#forward-button").click(function() {
+		clearInterval(intervalID);
+		nextImage($("#img-list li.active"));
+		if (autoPlay){
+			intervalID = setInterval("autoSlideShow()", slideShowInterval);
+		}
+	});
+	
+	intervalID = setInterval('autoSlideShow()', slideShowInterval);
 });
